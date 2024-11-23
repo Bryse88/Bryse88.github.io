5cc065e9bdce038ef0d9910f0ff94281ec5c5be3
bryse88 <bryseTurner0130@gmail.com>
2024-11-23 12:50:40 -0600

    Add updates for info-branch

--- src/Splash.jsx
+++ src/Splash.jsx
@@ -3,6 +3,7 @@ import Canvas from './splash/Canvas';
 import Nucleus from './splash/Nucleus';
 import Crest from './splash/Crest';
 
+
 const s = 100;
 const center = { x: s / 2, y: s / 2 };
 const circleColor = '#fff';
